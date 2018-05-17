53a4ab221d820e028142a926ea88625ef656bf7c
Daniel Bannert <prisis@users.noreply.github.com>
2018-05-17 15:02:55 +0200

    fixes #546 (#706)

--- Tests/Fixture/ErrorFixtureCommand.php
+++ Tests/Fixture/ErrorFixtureCommand.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace Viserio\Component\Exception\Tests\Fixtures;
+namespace Viserio\Component\Exception\Tests\Fixture;
 
 use Viserio\Component\Console\Command\Command;
 
